6a1ed2696adae650cad3b6776477a756ecd63ad5
Rutabadev <etienner37@gmail.com>
2024-01-07 02:41:25 +0100

    better button
    No more focus state on mouse click + tweak active colors

--- src/lib/StateControls.svelte
+++ src/lib/StateControls.svelte
@@ -7,7 +7,7 @@
   <div class="flex flex-wrap gap-4">
     {#each $windowsService.nextEvents.filter((state) => !state.includes("xstate")) as action}
       <button
-        class="select-none rounded border-2 border-gray-400 bg-gray-300 px-2 py-0.5 text-gray-900 outline-none ring-brand-400 focus:ring-2 active:bg-gray-500 active:text-gray-100 dark:border-gray-600 dark:bg-gray-400 dark:ring-brand-500  dark:active:bg-gray-700"
+        class="select-none rounded border-2 border-gray-400 bg-gray-300 px-2 py-0.5 text-gray-900 outline-none ring-brand-400 focus-visible:ring-2 active:translate-y-px active:bg-gray-400/60 dark:border-gray-600 dark:bg-gray-400  dark:ring-brand-500 dark:active:bg-gray-500"
         on:click={() => windowsService.send(action)}
       >
         {action}
